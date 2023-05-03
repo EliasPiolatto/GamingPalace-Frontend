3f072f62759fc63af2f5588c1ade66ecd36fc7bd
EliasPiolatto <eliaspiolatto77@hotmail.com>
2023-05-03 15:18:46 -0300

    deploy...

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { persistor, store } from './Redux/store';
 import Auth0ProviderWithHistory from './Auth0Provider/Auth0Provider';
 import axios from 'axios';
-axios.defaults.baseURL = 'https://gpback-production.up.railway.app/'
+// axios.defaults.baseURL = 'https://gpback-production.up.railway.app/'
+axios.defaults.baseURL = 'gaming-palace-backend-8iadqiz87-eliaspiolatto.vercel.app'
 
 
 
